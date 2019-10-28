f6103b4b94bf65c691b8801cd6d54b5ab9e8db02
wu-2273624 <57079811+wu-2273624@users.noreply.github.com>
2019-10-28 15:41:56 +0800

    Update SessionDemo.java
    修改了

--- src/com/qa/servlet/SessionDemo.java
+++ src/com/qa/servlet/SessionDemo.java
@@ -13,7 +13,8 @@ public class SessionDemo extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+		
+		System.out.println("修改");
 		response.setContenType("text/html; charset=UTF-8");
 		HttpSession session = request.getSession();
 		
